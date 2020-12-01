cec6ac677e45a6420b3fda49015f81bca7eac6de
Robert Pane <panerobert@yahoo.com>
2020-12-01 10:36:04 +0800

    In pretty good shape

--- quiz/models.py
+++ quiz/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Quiz(models.Model):
-	question = models.CharField(max_length = 500)
-	option1 = models.CharField(max_length = 20)
-	option2 = models.CharField(max_length = 20)
-	option3 = models.CharField(max_length = 20)
-	option4 = models.CharField(max_length = 20)
-	answer = models.CharField(max_length = 20)
+	question = models.TextField(max_length = 500)
+	option1 = models.TextField(max_length = 20)
+	option2 = models.TextField(max_length = 20)
+	option3 = models.TextField(max_length = 20)
+	option4 = models.TextField(max_length = 20)
+	answer = models.TextField(max_length = 20, null=True)
