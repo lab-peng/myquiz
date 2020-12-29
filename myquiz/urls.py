afe8e39ec1eeed67263da100e4420586067fc0ed
Robert Pane <panerobert@yahoo.com>
2020-12-29 17:50:55 +0800

    removed quizaip app

--- myquiz/urls.py
+++ myquiz/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from quiz import views as index_views
 from rest_framework.urlpatterns import format_suffix_patterns
-from quizapi import views
+from quiz import views
 
 urlpatterns = [
-	url(r'^$', index_views.index),
-	url(r'^login/$', index_views.login),
+	url(r'^$', views.index),
+	url(r'^login/$', views.login),
 	url(r'^questions/', include('quiz.urls')),
 	url(r'^quizapi/', views.QuizApiList.as_view()),
     url(r'^admin/', admin.site.urls),
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from quiz.models import Quiz
+from .serializers import QuizApiSerializer
 
+class QuizApiList(APIView):
+
+	def get(self, request):
+		quizapis = Quiz.objects.all()
+		serializer = QuizApiSerializer(quizapis, many = True)
+		return Response(serializer.data)
+
+	def post(self):
+		pass
+	
+	
 def qpage(request):
 	questions = Quiz.objects.all()
 
@@ -14,5 +29,3 @@ def questions(request):
 
 def login(request):
 	return render(request, 'quiz/login.html')
-	
-	
